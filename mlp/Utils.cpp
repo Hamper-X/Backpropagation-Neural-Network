54b6ac178e1d62da10adbff70c5c270b828d83a5
xXHachimanXx <globaldep90@gmail.com>
2020-10-02 09:27:03 -0300

    Criando classe de utilidades gerais.

---
+++ mlp/Utils.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "constants.h"
+#include "Utils.h"
+
+using namespace std;
+
+double Utils::BETA = MI;
+
+void Utils::gotoxy(int x, int y)
+{
+    printf("%c[%d;%df", 0x1B, y, x);
+}
+
+void Utils::gerarBaseDeDados()
+{
+    ofstream entrada, saida;
+    int a = 0, b = 0, c = 0;
+    int n = 10000;
+
+    entrada.open("../database/Entrada.txt");
+    saida.open("../database/Saida.txt");
+    srand(time(NULL));
+
+    for (int x = 0; x < LINHAS; x++)
+    {
+        a = rand() % n;
+        b = rand() % n;
+        c = a + b;
+        entrada << a << " " << b << endl;
+        saida << c << endl;
+    }
+
+    entrada.close();
+    saida.close();
+}
+
+void Utils::conferir()
+{
+    ifstream entrada, saida;
+    int a = 0, b = 0, c = 0;
+
+    entrada.open("Entrada.txt");
+    saida.open("Saida.txt");
+
+    for (int i = 0; i < LINHAS; i++)
+    {
+        entrada >> a >> b;
+        saida >> c;
+                
+        if (a + b != c)
+        {
+            printf("ENTRADAS INCONSISTENTES: linha %d, a = %d, b = %d, c = %d", i, a, b, c);
+            i = LINHAS;
+        }
+    }
+}
---
+++ mlp/Utils.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+#include <fstream>
+#include "constants.h"
+
+class Utils
+{
+    const int LINHAS = 10000;
+    public:        
+        static double BETA; // Fator de ajuste das correcoes
+        static void gotoxy(int x, int y);
+        void gerarBaseDeDados();
+        void conferir();
+};
