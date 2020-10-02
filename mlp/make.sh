b8ed67fa7e0d63ccb114f2038acb9bc16df64e26
xXHachimanXx <globaldep90@gmail.com>
2020-10-02 10:20:01 -0300

    Criando mais opções de compilação.

--- mlp/make.sh
+++ mlp/make.sh
@@ -1,11 +1,48 @@
 #!/bin/bash
 
-if [ "$1" == "-t" ]; then
-    g++ -D test -g -o back.exe \
+# if [ "$1" == "-t" ]; then
+#     g++ -D test -g -o back.exe \
+#         -I utils -I components components/*.cpp utils/*.cpp\
+#         back.cpp 
+# else
+#     g++ -g -o back.exe \
+#         -I utils -I components components/*.cpp utils/*.cpp\
+#         back.cpp
+# fi
+
+case "$1" in
+"-t"|"-T") g++ -D test -g -fopenmp -o back.exe \
         -I utils -I components components/*.cpp utils/*.cpp\
-        back.cpp
-else
-    g++ -g -o back.exe \
+        back.cpp ;;
+
+"-p"|"-P") g++ -g -fopenmp -o back.exe \
         -I utils -I components components/*.cpp utils/*.cpp\
-        back.cpp
-fi
+        back.cpp ;;
+
+"-o"|"-O") g++ -g -O3 -w -o back.exe \
+        -I utils -I components components/*.cpp utils/*.cpp\
+        back.cpp ;;
+
+"-tp"|"-TP") g++ -D test -g -fopenmp -o back.exe \
+        -I utils -I components components/*.cpp utils/*.cpp\
+        back.cpp ;;
+
+"-to"|"-TO") g++ -D test -w -g -O3 -o back.exe \
+        -I utils -I components components/*.cpp utils/*.cpp\
+        back.cpp ;;
+
+"-po"|"-PO") g++ -g -O3 -w -fopenmp -o back.exe \
+        -I utils -I components components/*.cpp utils/*.cpp\
+        back.cpp ;;
+
+"-tpo"|"-TPO") \
+        g++ -D test -g -O3 -fopenmp -o back.exe \
+        -I utils -I components components/*.cpp utils/*.cpp\
+        back.cpp ;;
+
+*) g++ -g -o back.exe \
+        -I utils -I components components/*.cpp utils/*.cpp\
+        back.cpp ;;
+esac
+
+
